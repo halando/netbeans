882434e3604481a7b4f684b2a60d6a7b8634e422
Andó Attila <andoattila698@gmail.com>
2023-10-13 15:52:42 +0200

    r

---
+++ src/meresiszimulacio/MeresiSzimulacio.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package meresiszimulacio;
+import java.awt.event.MouseAdapter;
+/**
+ *
+ * @author Tanar
+ */
+public class MeresiSzimulacio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FoAblak a=new FoAblak();
+        a.setVisible(true);
+    }
+    
+}
